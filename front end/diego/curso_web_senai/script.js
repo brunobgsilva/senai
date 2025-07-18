78f688dd68c60a676b8ac13f81f9151622edabee
Bruno <bruno_bg_silva@estudante.sesisenai.org.br>
2025-07-17 21:57:37 -0300

    dia 17-07-25

--- front end/diego/curso_web_senai/script.js
+++ front end/diego/curso_web_senai/script.js
@@ -1,24 +1,55 @@
-function openOrCloseSection() {
 
-}
-
+// handle accordion menus
 document.querySelectorAll('.section')
     .forEach((section) => {
         section.querySelector('.section-title')
             .addEventListener('click', (e) => {
                 const content = section.querySelector('.section-content');
 
-                if (content.style.display == 'none') {
+                if (!content.style.display) {
                     content.style.display = 'block';
                 } else {
-                    content.style.display = 'none';
+                    content.style.display = '';
                 }
 
-                // if (content.style.maxHeight) {
-                //     content.style.maxHeight = null;
-                // } else {
-                //     content.style.maxHeight = '1000px';
-                // }
+                // make transition work
+                if (!content.style.maxHeight) {
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                } else {
+                    content.style.maxHeight = '';
+                }
+
+                e.target.classList.toggle('active-section');
 
         });
     });
+    
+let previousScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+
+    const header = document.querySelector('.header');
+    const headerBottom = header.offsetTop + header.offsetHeight;
+
+    const footer = document.querySelector('.footer');
+
+    if (previousScrollPos >= currentScrollPos ||currentScrollPos < headerBottom + 50) {
+        header.style.top = '0';
+    } else {
+        header.style.top = '-3rem';
+    }
+
+    // if (currentScrollPos >= previousScrollPos) {
+    //     header.style.top = '-3rem';
+    //     footer.style.bottom = '0';
+    // } else {
+    //     header.style.top = '0';
+    //     footer.style.bottom = '-3rem';
+    // }
+
+    previousScrollPos = currentScrollPos;
+
+}
+
+
