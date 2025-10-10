ce08ea8d2041f2c02f85c86ac8fe21f5fdc0573d
Bruno Barreto <bruno_bg_silva@estudante.sesisenai.org.br>
2025-10-09 21:51:08 -0300

    dia 09/10/25

---
+++ front end/gustavo hoffmann/aula 14/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('botao-cadastrar')  
+    .addEventListener('click', () => {
+        alert('Dados cadastrados!')
+    });
--- tesste/server.js
+++ tesste/server.js
@@ -1,24 +1,55 @@
 const http = require('http');
 const PORT = 8484;
 
-const data = [];
+const dados = [{nome: 'caalango'}];
 
 http.createServer((req, res) => {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    if (req.url == '/oi') {
-        res.writeHead(200, { "content-type": "text/plain" })
-        console.log('oi');
-        res.write(JSON.stringify({nome: 'Sabao', idade: 12}))
-        res.end('oie');
-    }
+    if (req.url == '/api/data/') {
 
-    if (req.url == '/api/') {
-        res.writeHead(201, { "content-type": "application/json" })
-        console.log('postado');
-        req.
-        res.end('oie');
+        console.log("Acessaram com request " + req.method)
+
+        if (req.method === 'OPTIONS') {
+            res.setHeader('Access-Control-Allow-Methods', '*');
+            res.setHeader('Access-Control-Allow-Headers', '*'); // Include other necessary headers
+            res.writeHead(204); // 204 No Content is standard for successful OPTIONS
+            res.end();
+            return;
+        }
+
+        if (req.method === 'POST') {
+
+            console.log('oi post');
+
+            let body = '';
+
+            req.on('data', (chunk) => {
+
+                console.log(chunk);
+                console.log(chunk);
+                body += chunk;
+
+            });
+
+            req.on('end', () => {
+                dados.push(JSON.parse(body));
+                res.writeHead(201, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(dados));
+            });
+
+            return;
+
+        }
+
+        if (req.method === 'GET') {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(dados));
+
+            return;
+        }
+
     }
 
 }).listen(PORT);
