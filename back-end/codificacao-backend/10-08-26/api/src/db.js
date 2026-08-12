ab55fd4789cc1b283c7b0552af1ed39bc7dcd743
bruno <bruno_bg_silva@estudante.sesisenail.org.br>
2026-08-11 21:59:51 -0300

    dia 11-08-26 Fazer conexao com banco de dados denovo e alguns queries

--- back-end/codificacao-backend/10-08-26/api/src/db.js
+++ back-end/codificacao-backend/10-08-26/api/src/db.js
@@ -1,15 +1,49 @@
 import 'dotenv/config'
-import pg from 'pg';
-import { Pool } from pg;
+import { Pool } from 'pg';
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER
-});
+async function connect() {
+    if (global.connection) {
+        return global.connection.connect();
+    };
 
-pool.on('error', (err, client) => {
-    console.error('Erro no client', err);
-    process.exit(-1);
-});
+    const pool = new Pool({
+        host: process.env.DB_HOST,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT
+    });
+
+    const client = await pool.connect();
+    if (client) {
+        console.log("Criou o pool de conexao")
+    };
+    pool.on('error', (err, client) => {
+        console.error('Erro no client', err);
+        process.exit(-1);
+    });
+
+    const res = await client.query("SELECT now()");
+    console.log(`Tempo do banco: ${JSON.stringify(res.rows[0])}`);
+    client.release();
+
+    global.connection = pool;
+    return pool.connect()
+};
+
+export async function selectClientes() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM cliente");
+    return res.rows;
+}
+
+export async function selectClienteByID(id) {
+    const client = await connect();
+    const res = await client.query(
+        `SELECT * FROM cliente WHERE id_cli = $1;`,
+        [id]
+    );
+    return res.rows;
+}
+
+connect();
--- back-end/codificacao-backend/10-08-26/api/src/server.js
+++ back-end/codificacao-backend/10-08-26/api/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import * as db from "./db.js";
 
 const port = process.env.API_PORT;
 const app = express();
@@ -11,6 +12,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     return res.status(200).json(res.status)
 });
+
+app.get('/clientes', async (req, res) => {
+    const clientes = await db.selectClientes();
+    return res.status(200).json(clientes);
+});
+
+app.get('/clientes/:id', async (req, res) => {
+    const id = req.params.id
+    const cliente = await db.selectClienteByID(id);
+    return res.status(200).json(cliente);
+})
 
 app.listen(port, () => {
     console.log(`API Rodando em: http://localhost:${port}`);
